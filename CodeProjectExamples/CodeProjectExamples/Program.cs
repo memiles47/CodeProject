a5e1bd38d30c99cfa0187a8ed8704283879cf624
Michael E Miles <michael@miles-me.net>
2016-06-01 09:19:59 -0500

    Confirms my thoughts that s2.length = 0. Not 1 as indicated on CodeProject

---
+++ CodeProjectExamples/CodeProjectExamples/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CodeProjectExamples
+{
+    class Program
+    {
+        static void Main()
+        {
+            var s1 = string.Empty;
+            var s2 = "";
+
+            Console.WriteLine($"The Length of s1 is: {s1.Length}");
+            Console.WriteLine($"The Length of s2 is: {s2.Length}");
+        }
+    }
+}
